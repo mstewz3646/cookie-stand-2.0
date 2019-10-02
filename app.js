77f615e5d7f5b6070dc32ec27cbee8de7be8a09a
Matt <mstewz3646@gmail.com>
2019-10-02 15:55:34 -0700

    added another city with the coding.

--- app.js
+++ app.js
@@ -45,13 +45,53 @@ var Seattle = {
     ul.appendChild(li);
     }
 };
+
+var Tokyo = {
+  location: 'Tokyo',
+  minCust: 3,
+  maxCust: 25,
+  avgCookies: 1.2,
+  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  
+  randomNumCustomer: function() {
+      return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  cookiesPurchased: function () {
+      var result = [];
+      for (var i = 0; i < this.storeHours.length; i++) {
+        result.push(Math.floor(this.randomNumCustomer() * this.aveCookies));
+      }
+      return result;
+  },
+  totalSum: function () {
+      var cookieSum = 0;
+      for (var i = 0; i <= this.storeHours.length; i++) {
+        cookieSum += this.cookiesPurchased()[0];
+      }
+      return cookieSum.toLocaleString();
+  },
+  render: function() {
+      var section = document.getElementById('container');
+      var ul = document.createElement('ul');
+      ul.className = 'cookies';
+      ui.innerHTML = `<h2>${this.location}</h2>`;  //will need to be changed. Check around 40min mark
+      section.appendChild(ul);
+      
+      for (var i = 0; i <= this.storeHours.length -1; i++) {
+          console.log('hours', this.storeHours[i]);
+          var li = document.createElement('li');
+          li.textContent = `
+          ${this.storeHours[i]}: $(this.cookiesPurchased()[i]) cookies
+          `;
+      ul.appendChild(li);
+  }
+  li.textContent = `Total: ${this.totalSum()} cookies`;
+  ul.appendChild(li);
+  }
+};
   
 
-// var Tokyo = {
-//     min: 3,
-//     max: 25,
-//     avg: 1.2
-// }
+
 
 // var Dubai = {
 //     min: 11,
