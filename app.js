b26816fc1d1811ab930ca133f46095bf65d89dbe
Matt <mstewz3646@gmail.com>
2019-10-02 21:21:33 -0700

    completed a base code for all stores

--- app.js
+++ app.js
@@ -89,31 +89,138 @@ var Tokyo = {
   ul.appendChild(li);
   }
 };
+
+
+var Dubai = {
+  location: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  
+  randomNumCustomer: function() {
+      return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  cookiesPurchased: function () {
+      var result = [];
+      for (var i = 0; i < this.storeHours.length; i++) {
+        result.push(Math.floor(this.randomNumCustomer() * this.aveCookies));
+      }
+      return result;
+  },
+  totalSum: function () {
+      var cookieSum = 0;
+      for (var i = 0; i <= this.storeHours.length; i++) {
+        cookieSum += this.cookiesPurchased()[0];
+      }
+      return cookieSum.toLocaleString();
+  },
+  render: function() {
+      var section = document.getElementById('container');
+      var ul = document.createElement('ul');
+      ul.className = 'cookies';
+      ui.innerHTML = `<h2>${this.location}</h2>`;  //will need to be changed. Check around 40min mark
+      section.appendChild(ul);
+      
+      for (var i = 0; i <= this.storeHours.length -1; i++) {
+          console.log('hours', this.storeHours[i]);
+          var li = document.createElement('li');
+          li.textContent = `
+          ${this.storeHours[i]}: $(this.cookiesPurchased()[i]) cookies
+          `;
+      ul.appendChild(li);
+  }
+  li.textContent = `Total: ${this.totalSum()} cookies`;
+  ul.appendChild(li);
+  }
+};
   
 
-
-
-// var Dubai = {
-//     min: 11,
-//     max: 38,
-//     avg: 3.7
-// }
-
-// var Paris = {
-//     min: 20,
-//     max: 38,
-//     avg: 2.3,
-// }
-
-// var Lima = {
-//     min: 2,
-//     max: 16,
-//     avg: 4.6
-// }
-
-
-// function randomCust(min, max) {
-//     return (Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-//   }
+var Paris = {
+  location: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   
-//   console.log(randomCust(23, 65));
+  randomNumCustomer: function() {
+      return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  cookiesPurchased: function () {
+      var result = [];
+      for (var i = 0; i < this.storeHours.length; i++) {
+        result.push(Math.floor(this.randomNumCustomer() * this.aveCookies));
+      }
+      return result;
+  },
+  totalSum: function () {
+      var cookieSum = 0;
+      for (var i = 0; i <= this.storeHours.length; i++) {
+        cookieSum += this.cookiesPurchased()[0];
+      }
+      return cookieSum.toLocaleString();
+  },
+  render: function() {
+      var section = document.getElementById('container');
+      var ul = document.createElement('ul');
+      ul.className = 'cookies';
+      ui.innerHTML = `<h2>${this.location}</h2>`;  //will need to be changed. Check around 40min mark
+      section.appendChild(ul);
+      
+      for (var i = 0; i <= this.storeHours.length -1; i++) {
+          console.log('hours', this.storeHours[i]);
+          var li = document.createElement('li');
+          li.textContent = `
+          ${this.storeHours[i]}: $(this.cookiesPurchased()[i]) cookies
+          `;
+      ul.appendChild(li);
+  }
+  li.textContent = `Total: ${this.totalSum()} cookies`;
+  ul.appendChild(li);
+  }
+};
+
+
+var Lima = {
+  location: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  
+  randomNumCustomer: function() {
+      return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  cookiesPurchased: function () {
+      var result = [];
+      for (var i = 0; i < this.storeHours.length; i++) {
+        result.push(Math.floor(this.randomNumCustomer() * this.aveCookies));
+      }
+      return result;
+  },
+  totalSum: function () {
+      var cookieSum = 0;
+      for (var i = 0; i <= this.storeHours.length; i++) {
+        cookieSum += this.cookiesPurchased()[0];
+      }
+      return cookieSum.toLocaleString();
+  },
+  render: function() {
+      var section = document.getElementById('container');
+      var ul = document.createElement('ul');
+      ul.className = 'cookies';
+      ui.innerHTML = `<h2>${this.location}</h2>`;  //will need to be changed. Check around 40min mark
+      section.appendChild(ul);
+      
+      for (var i = 0; i <= this.storeHours.length -1; i++) {
+          console.log('hours', this.storeHours[i]);
+          var li = document.createElement('li');
+          li.textContent = `
+          ${this.storeHours[i]}: $(this.cookiesPurchased()[i]) cookies
+          `;
+      ul.appendChild(li);
+  }
+  li.textContent = `Total: ${this.totalSum()} cookies`;
+  ul.appendChild(li);
+  }
+};
